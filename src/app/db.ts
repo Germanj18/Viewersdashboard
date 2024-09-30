497cef6645ebb0ecb013f4d699a76f562715b68c
Germnj18 <germnaguilera@gmail.com>
2024-09-30 11:30:30 -0300

    cambios ahora sisi

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,6 +1,10 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: "postgres://default:SbPtsfC23mND@ep-muddy-rain-a4kexquv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require?sslmode=require",
-})
+  connectionString: process.env.POSTGRES_PRISMA_URL || "postgres://default:SbPtsfC23mND@ep-muddy-rain-a4kexquv-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+});
+
+export default pool;
