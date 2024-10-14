c0b58de76ef759992b2a01662e976656520bb863
Germnj18 <germnaguilera@gmail.com>
2024-10-14 17:56:52 -0300

    Cambios

--- src/app/components/ProgramCards.tsx
+++ src/app/components/ProgramCards.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
-import { format, startOfMonth, endOfMonth, subMonths, addMonths, eachDayOfInterval, isSunday, getDay } from 'date-fns';
+import { format, startOfMonth, endOfMonth, subMonths, addMonths, eachDayOfInterval, getDay } from 'date-fns';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler);
 
@@ -48,7 +48,7 @@ const ProgramCards: React.FC<{ theme: string }> = ({ theme }) => {
     const filteredData = data.filter(item => {
       const date = new Date(item.fecha);
       const day = getDay(date);
-      return day !== 0 && day !== 6; // Excluir domingos (0) y sábados (6)
+      return day >= 1 && day <= 5; // Incluir solo lunes (1) a viernes (5)
     });
 
     return filteredData.map(item => {
